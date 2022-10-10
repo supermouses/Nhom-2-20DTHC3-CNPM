5a68062a1abbab5bfe011319165c865c799f4b52
almaThanh <113413554+almaThanh@users.noreply.github.com>
2022-10-11 01:24:26 +0700

    Hoàn Chỉnh Form Nhân Viên

---
+++ Hoàn Chỉnh quản lý Nhân Viên/NhaTroBoTu/NhaTroBoTu/ThemNhanVien.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NhaTroBoTu
+{
+    public partial class ThemNhanVien : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        string str = "Data Source=LAPTOP-AA8F4MMK;Initial Catalog=QuanLyNhaTroBoTu;Integrated Security=True";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        public ThemNhanVien()
+        {
+            InitializeComponent();
+        }
+        void loaddata()
+        {
+            cmd = conn.CreateCommand();
+            cmd.CommandText = "select MaNV,TenNV,SĐT,DiaChi,NgaySinh,GioiTinh,TenCV,Luong from NhanVien,ChucVuNV where NhanVien.MaCV = ChucVuNV.MaCV";
+            adapter.SelectCommand = cmd;
+            dt.Clear();
+            adapter.Fill(dt);
+            dataThemNhanVien.DataSource = dt;
+        }
+
+        private void ThemNhanVien_Load(object sender, EventArgs e)
+        {
+            DataTable table1 = new DataTable();
+            conn = new SqlConnection(str);
+            cmd = conn.CreateCommand();
+            cmd.CommandText = "select * from ChucVuNV";
+            adapter.SelectCommand = cmd;
+            adapter.Fill(table1);
+            cmbThemCV.DataSource = table1;
+            cmbThemCV.DisplayMember = "TenCV";
+            cmbThemCV.ValueMember = "MaCV";
+            cmbThemCV.SelectedItem = null;
+            cmbThemCV.Text = "";
+            radThemKhac.Checked = true;
+            conn.Open();
+            loaddata();
+        }
+
+        private void dataThemNhanVien_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = dataThemNhanVien.CurrentRow.Index;
+            txtThemMa.Text = dataThemNhanVien.Rows[i].Cells[0].Value.ToString();
+            txtThemTen.Text = dataThemNhanVien.Rows[i].Cells[1].Value.ToString();
+            txtThemSDT.Text = dataThemNhanVien.Rows[i].Cells[2].Value.ToString();
+            txtThemDiaChi.Text = dataThemNhanVien.Rows[i].Cells[3].Value.ToString();
+            datimeThemNgayS.Text = dataThemNhanVien.Rows[i].Cells[4].Value.ToString();
+            if (dataThemNhanVien.Rows[i].Cells[5].Value.ToString() == "Nam")
+            {
+                radThemNam.Checked = true;
+            }
+            else if (dataThemNhanVien.Rows[i].Cells[5].Value.ToString() == "Nữ")
+            {
+                radThemNu.Checked = true;
+
+            }
+            else
+            {
+                radThemKhac.Checked = true;
+            }
+            string ChucVuNV = dataThemNhanVien.CurrentRow.Cells[6].Value.ToString();
+            cmbThemCV.Text = ChucVuNV;
+            txtThemLuong.Text = dataThemNhanVien.Rows[i].Cells[7].Value.ToString();
+        }
+
+        private void btnOkThemNV_Click(object sender, EventArgs e)
+        {
+            if (txtThemMa.Text == "" || txtThemTen.Text == "" || txtThemSDT.Text == ""|| txtThemDiaChi.Text == ""|| txtThemLuong.Text == "")
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!!", "Thông Báo", MessageBoxButtons.OK);
+            else
+            {
+                try
+                {
+                    cmd = conn.CreateCommand();
+                    string gt;
+                    if (radThemNam.Checked)
+                    {
+                        gt = "Nam";
+                    }
+                    else if (radThemNu.Checked)
+                    {
+                        gt = "Nữ";
+                    }
+                    else
+                    {
+                        gt = "Khác";
+                    }
+                    cmd.CommandText = "insert into NhanVien values('" + txtThemMa.Text + "' , N'" + txtThemTen.Text + "' , '" + txtThemSDT.Text + "' ,N'" + txtThemDiaChi.Text + "', '" + datimeThemNgayS.Value.ToString() + "', N'" + gt + "' ,N'" + cmbThemCV.SelectedValue.ToString() + "' ,N'" + txtThemLuong.Text + "')";
+                    cmd.ExecuteNonQuery();
+                    loaddata();
+                    MessageBox.Show("Thêm dữ liệu thành công!", "Thông Báo", MessageBoxButtons.OK);
+                }
+                catch
+                {
+                    MessageBox.Show("Mã bị trùng!!! Vui lòng nhập mã khác!!!");
+                }
+            }     
+        }
+
+        private void btnCanelThemNV_Click(object sender, EventArgs e)
+        {
+            QuanLyNhanVien qlnv = new QuanLyNhanVien();
+            Hide();
+            qlnv.Show();
+            this.Close();
+        }
+
+        private void dataThemNhanVien_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = dataThemNhanVien.CurrentRow.Index;
+            txtThemMa.Text = dataThemNhanVien.Rows[i].Cells[0].Value.ToString();
+            txtThemTen.Text = dataThemNhanVien.Rows[i].Cells[1].Value.ToString();
+            txtThemSDT.Text = dataThemNhanVien.Rows[i].Cells[2].Value.ToString();
+            txtThemDiaChi.Text = dataThemNhanVien.Rows[i].Cells[3].Value.ToString();
+            datimeThemNgayS.Text = dataThemNhanVien.Rows[i].Cells[4].Value.ToString();
+            if (dataThemNhanVien.Rows[i].Cells[5].Value.ToString() == "Nam")
+            {
+                radThemNam.Checked = true;
+            }
+            else if (dataThemNhanVien.Rows[i].Cells[5].Value.ToString() == "Nữ")
+            {
+                radThemNu.Checked = true;
+
+            }
+            else
+            {
+                radThemKhac.Checked = true;
+            }
+            string ChucVuNV = dataThemNhanVien.CurrentRow.Cells[6].Value.ToString();
+            cmbThemCV.Text = ChucVuNV;
+            txtThemLuong.Text = dataThemNhanVien.Rows[i].Cells[7].Value.ToString();
+        }
+    }
+}
---
+++ Hoàn Chỉnh quản lý Nhân Viên/NhaTroBoTu/NhaTroBoTu/TimKiemNhanVien.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace NhaTroBoTu
+{
+    partial class TimKiemNhanVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataTimNhanVien = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTim = new System.Windows.Forms.TextBox();
+            this.cmbTim = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTimNhanVien)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 27.75F);
+            this.label1.Location = new System.Drawing.Point(331, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(464, 62);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Tìm Kiếm Nhân Viên ";
+            // 
+            // dataTimNhanVien
+            // 
+            this.dataTimNhanVien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataTimNhanVien.Location = new System.Drawing.Point(70, 273);
+            this.dataTimNhanVien.Name = "dataTimNhanVien";
+            this.dataTimNhanVien.RowHeadersWidth = 51;
+            this.dataTimNhanVien.RowTemplate.Height = 24;
+            this.dataTimNhanVien.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataTimNhanVien.Size = new System.Drawing.Size(986, 220);
+            this.dataTimNhanVien.TabIndex = 24;
+            this.dataTimNhanVien.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataTimNhanVien_CellClick);
+            this.dataTimNhanVien.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataTimNhanVien_CellContentClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtTim);
+            this.groupBox1.Controls.Add(this.cmbTim);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(70, 151);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(986, 98);
+            this.groupBox1.TabIndex = 26;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tìm kiếm thông tin nhân viên";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(419, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 22);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Từ tìm:";
+            // 
+            // txtTim
+            // 
+            this.txtTim.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTim.Location = new System.Drawing.Point(497, 36);
+            this.txtTim.Name = "txtTim";
+            this.txtTim.Size = new System.Drawing.Size(324, 28);
+            this.txtTim.TabIndex = 2;
+            this.txtTim.TextChanged += new System.EventHandler(this.txtTim_TextChanged);
+            // 
+            // cmbTim
+            // 
+            this.cmbTim.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbTim.FormattingEnabled = true;
+            this.cmbTim.Items.AddRange(new object[] {
+            "Mã Nhân Viên",
+            "Tên Nhân Viên"});
+            this.cmbTim.Location = new System.Drawing.Point(176, 36);
+            this.cmbTim.Name = "cmbTim";
+            this.cmbTim.Size = new System.Drawing.Size(179, 28);
+            this.cmbTim.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 20);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Tìm kiếm theo:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(954, 499);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 43);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Quay lại";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // TimKiemNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1125, 554);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataTimNhanVien);
+            this.Controls.Add(this.label1);
+            this.Name = "TimKiemNhanVien";
+            this.Text = "TimKiemNhanVien";
+            this.Load += new System.EventHandler(this.TimKiemNhanVien_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataTimNhanVien)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataTimNhanVien;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTim;
+        private System.Windows.Forms.ComboBox cmbTim;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
